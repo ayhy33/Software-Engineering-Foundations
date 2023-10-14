a41b58c2ba67a86899139fecf5bb74ed0bd60754
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-10-14 22:58:51 +0100

    Update Printvariables.java

--- variables/Printvariables.java
+++ variables/Printvariables.java
@@ -2,5 +2,10 @@ public class Printvariables {
  public static void main (String [] args) {
    String name = "John";
    System.out.println("Hello" + name);
+
+  String firstName = "John";
+  String lastName = "Doe";
+  String fullName = firstName + lastName;
+  System.out.println(fullName);
  }
 }
