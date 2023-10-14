c0da5134bf762d5d80628de7832f7e5bb092212b
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-10-14 22:06:14 +0100

    Update Main.java

--- syntax/Main.java
+++ syntax/Main.java
@@ -10,5 +10,14 @@ public class Main {
     // print() will not insert a new line at the end of output
     System.out.print("Hello World! ");
     System.out.print("I will print on the same line.");
+
+    // numbers do not need quotation marks
+    System.out.println(3);
+    System.out.println(358);
+    System.out.println(50000);
+
+    // mathematical calculations
+    System.out.println(3 + 3);
+    System.out.println(2 * 5);
   }
 }
