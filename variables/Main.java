56b792b2b88204bb50c7d42871541628d8a63957
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-10-14 22:41:13 +0100

    Update Main.java

--- variables/Main.java
+++ variables/Main.java
@@ -1,6 +1,17 @@
 public class Main {
   public static void main (String [] args) {
+
+    // variable that store text
     String name = "John";
     System.out.println(name);
+
+    // variable that store number
+    int myNum = 15;
+    System.out.println(myNum);
+
+    // variable without assigning value and assign value later
+    int myNum;
+    myNum = 15;
+    System.out.println(myNum);
   }
 }
