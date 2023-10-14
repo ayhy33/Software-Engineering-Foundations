dd69fa7636ef466e1ac20aff062ddbf3437d88d7
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-10-14 21:58:47 +0100

    Update Main.java

--- syntax/Main.java
+++ syntax/Main.java
@@ -2,6 +2,13 @@ public class Main {
   public static void main (String [] args) {
     System.out.println("Hello World");
     System.out.println("I am learning Java");
-    System.out.println("It is awesome!")
+    System.out.println("It is awesome!");
+    
+    // text must be wrapped inside double quotation marks
+    System.out.println(This sentence will produce an error);
+
+    // print() will not insert a new line at the end of output
+    System.out.print("Hello World!");
+    System.out.print("I will print on the same line.");
   }
 }
