a0acd36e1fc2159278cf262a9b81635d4aa8807a
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-10-15 00:18:27 +0100

    Update main.java

--- casting/main.java
+++ casting/main.java
@@ -1,9 +1,18 @@
 public class Main {
   public static void main (String [] args) {
+
+    // widening casting
     int myInt = 9;
     double myDouble = myInt; // Automatic casting: int to double
     
     System.out.println(myInt);
     System.out.println(myDouble);
+
+    //narrowing casting
+    double myDoubled = 9.78d;
+    int myIntg = (int) myDoubled; // Explicit casting: double to int
+
+    System.out.println(myDoubled);
+    System.out.println(myIntg);
   }
 }
