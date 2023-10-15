3921ad2434b5969ae544bb0fa8de9839fff7630d
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-10-15 22:18:36 +0100

    Update Main.java

--- strings/Main.java
+++ strings/Main.java
@@ -6,5 +6,14 @@ public class Main {
     // string length
     String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     System.out.println("The length of the txt string is: " + txt.length());
+
+    // upper and lower case string
+    String text = "Hello World";
+    System.out.println(text.toUpperCase());
+    System.out.println(text.toLowerCase());
+
+    // index 
+    String tex = "Please locate where 'locate' occurs!";
+    System.out.println(tex.indexOf("locate"));
   }
 }
