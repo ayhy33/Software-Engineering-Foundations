495954c28ffa5519c9ba338432c1c5b149036126
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-10-15 22:10:38 +0100

    Update Main.java

--- strings/Main.java
+++ strings/Main.java
@@ -2,5 +2,9 @@ public class Main {
   public static void main (String [] args) {
     String greeting = "Hello";
     System.out.println(greeting);
+
+    // string length
+    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    System.out.println("The length of the txt string is: " + txt.length());
   }
 }
