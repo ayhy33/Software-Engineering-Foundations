2f682ba7d20d406fc6be64ffa79f8b8e214f300d
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-10-15 23:36:41 +0100

    Update Main.java

--- strings/Main.java
+++ strings/Main.java
@@ -14,6 +14,43 @@ public class Main {
 
     // index 
     String tex = "Please locate where 'locate' occurs!";
-    System.out.println(tex.indexOf("locate"));
+    System.out.println(tex.indexOf("locate")); // Java starts from index 0
+
+    // concatenation
+    String firstName = "John";
+    String lastName = "DOe";
+    System.out.println(firstName + " " + lastName);
+
+    // concat()
+    String firstNm = "John";
+    String lastNm = "Doe";
+    System.out.println(firstNm.concat(lastNm));
+
+    // adding two numbers
+    int x = 10;
+    int y = 20;
+    int z = x + y; // z will be 30 (an integer)
+
+    // adding two strings
+    String p = "10";
+    String q = "20";
+    String r = p + q; // r will be 1020 (a String)
+
+    // adding number and string
+    String t = "10";
+    int u = 20;
+    String v = t + u; // v will be 1020 (a String)
+
+    // \" inserts a double quote 
+    String statement = "We are the so-called \"Vikings\" from the north.";
+    System.out.println(statement);
+
+    // \' inserts a single quote
+    String sentence = "It\'s alright.";
+    System.out.println(sentence);
+
+    // \\ inserts a single backslash
+    String line = "The character \\ is called backslash.";
+    System.out.println(line);
   }
 }
